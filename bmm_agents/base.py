961f6a8dd074ece34a8035c942f8300082c21051
maffettone <phillip.maffettone@gmail.com>
2023-03-23 08:13:36 -0400

    BUG: API KEY

--- bmm_agents/base.py
+++ bmm_agents/base.py
@@ -233,7 +233,7 @@ class BMMBaseAgent(Agent, ABC):
             config_file_path="/etc/bluesky/kafka.yml"
         )
         qs = REManagerAPI(http_server_uri=f"https://qserver.nsls2.bnl.gov/{beamline_tla}")
-        qs.set_authorization_key(api_key=None)
+        qs.set_authorization_key(api_key="zzzzz")
 
         kafka_consumer = AgentConsumer(
             topics=[
